a9b30d25339df6ee6cc519404349d1a46505f810
Danny Rosenblatt <dgrosenblatt@gmail.com>
2022-12-02 23:43:37 -0500

    Design updates pt. 2

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -16,6 +16,7 @@ import { MEDIUM_SCREEN, PAGES } from "../../utils/constants";
 import { dollars, transactionDate } from "../../utils";
 import * as Api from "../../api";
 import Maybe from "../../components/_shared/Maybe";
+import PlainBox from "../../components/_design_system/PlainBox";
 
 const Portfolio = ({
   onSellOpen,
@@ -40,74 +41,57 @@ const Portfolio = ({
 
   return (
     <Box>
-      <Box
-        bgColor="white"
-        borderWidth="1px"
-        borderRadius="lg"
-        marginBottom="1rem"
-        padding="2"
-      >
-        <Heading textAlign="center" marginBottom={"1em"}>
-          Portfolio Overview
+      <Heading textAlign="center">Portfolio Overview</Heading>
+      <Profile
+        onSellOpen={onSellOpen}
+        setPrefillSellHoldingId={setPrefillSellHoldingId}
+        user={user}
+      />
+      <PlainBox>
+        <Heading size="md" marginBottom="1em">
+          Transactions
         </Heading>
-        <Profile
-          onSellOpen={onSellOpen}
-          setPrefillSellHoldingId={setPrefillSellHoldingId}
-          user={user}
-        />
-        <Box
-          bgColor="white"
-          borderWidth="1px"
-          borderRadius="lg"
-          marginTop="2"
-          marginBottom="1rem"
-          padding="2"
-        >
-          <Heading size="md" marginBottom="1em">
-            Transactions
-          </Heading>
-          <Table variant="striped" colorScheme="gray">
-            {isLargerThanMd && (
-              <Thead>
-                <Tr>
-                  <Th>Date</Th>
-                  <Th>Event</Th>
-                  <Th>Amount</Th>
-                  <Th>Notes</Th>
-                </Tr>
-              </Thead>
-            )}
-            <Tbody>
-              {balanceTransactions.map((balanceTransaction) =>
-                isLargerThanMd ? (
-                  <>
-                    <Tr key={balanceTransaction.id}>
-                      <Td>{transactionDate(balanceTransaction.created_at)}</Td>
-                      <Td>{balanceTransaction.event}</Td>
-                      <Td>{dollars(balanceTransaction.amount)}</Td>
-                      <Td>{balanceTransaction.notes}</Td>
-                    </Tr>
-                  </>
-                ) : (
+        <Table variant="striped" colorScheme="gray">
+          {isLargerThanMd && (
+            <Thead>
+              <Tr>
+                <Th>Date</Th>
+                <Th>Event</Th>
+                <Th>Amount</Th>
+                <Th>Notes</Th>
+              </Tr>
+            </Thead>
+          )}
+          <Tbody>
+            {balanceTransactions.map((balanceTransaction) =>
+              isLargerThanMd ? (
+                <>
                   <Tr key={balanceTransaction.id}>
-                    <Td>
-                      Date: {transactionDate(balanceTransaction.created_at)}
-                      <br />
-                      Event: {balanceTransaction.event}
-                      <br />
-                      Amount: {dollars(balanceTransaction.amount)}
-                      <br />
-                      <Maybe value={balanceTransaction.notes}>
-                        Notes: {balanceTransaction.notes}
-                      </Maybe>
-                    </Td>
+                    <Td>{transactionDate(balanceTransaction.created_at)}</Td>
+                    <Td>{balanceTransaction.event}</Td>
+                    <Td>{dollars(balanceTransaction.amount)}</Td>
+                    <Td>{balanceTransaction.notes}</Td>
                   </Tr>
-                )
-              )}
-            </Tbody>
-          </Table>
-        </Box>
-      </Box>
+                </>
+              ) : (
+                <Tr key={balanceTransaction.id}>
+                  <Td>
+                    Date: {transactionDate(balanceTransaction.created_at)}
+                    <br />
+                    Event: {balanceTransaction.event}
+                    <br />
+                    Amount: {dollars(balanceTransaction.amount)}
+                    <br />
+                    <Maybe value={balanceTransaction.notes}>
+                      Notes: {balanceTransaction.notes}
+                    </Maybe>
+                  </Td>
+                </Tr>
+              )
+            )}
+          </Tbody>
+        </Table>
+      </PlainBox>
     </Box>
   );
 };
